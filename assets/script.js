48af1ee3b1e3d41381ecb5cee1ba1941d0094e68
Leon Forsythe <lvforsythe95@gmail.com>
2022-11-11 09:03:41 -0500

    added high score form function

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,10 @@
 var quizContainer = document.querySelector("main");
 var startbtn = document.getElementById("start-btn");
 var questionSection = document.createElement("section");
-var currentQuestion = 0;
 var timer = 90;
+var scoreForm = document.createElement("form");
+var currentQuestion = 0;
+
 var quiz = [
   {
     question: "What color is the sun?",
@@ -21,25 +23,34 @@ var quiz = [
   },
 ];
 
-//checks chosen answer 
+//creates score form after game is over
+function createForm() {
+  scoreForm.innerHTML = `<form action="./scores.html">
+<p>Your score is ${timer}</p>
+<label>Enter your initials:</label>
+<input type="text"></input>
+</form>`;
+}
+
+//checks chosen answer
 function checkAnswer(event) {
   var selectedAnswer = event.target.textContent;
 
-  if(quiz[currentQuestion].correctAnswer === selectedAnswer)
-  {
-    console.log("correct!")
-    currentQuestion++
+  if (quiz[currentQuestion].correctAnswer === selectedAnswer) {
+    console.log("correct!");
+    currentQuestion++;
   } else {
-    console.log("incorrect")
+    console.log("incorrect");
     timer -= 15;
-    console.log(timer)
+    console.log(timer);
   }
   renderQuiz();
 }
 
 //creates quiz element and adds it to page in place of previous element
 function renderQuiz() {
-  questionSection.innerHTML = `<h1>${quiz[currentQuestion].question}</h1>
+  if (currentQuestion < quiz.length) {
+    questionSection.innerHTML = `<h1>${quiz[currentQuestion].question}</h1>
           <ul>
               <li>${quiz[currentQuestion].answers[2]}</li>
               <li>${quiz[currentQuestion].answers[0]}</li>
@@ -48,11 +59,16 @@ function renderQuiz() {
           </ul>
   `;
 
-  quizContainer.appendChild(questionSection);
+    quizContainer.appendChild(questionSection);
 
-  var answerElements = document.querySelectorAll("li");
-  for (let x = 0; x < answerElements.length; x++) {
-    answerElements[x].addEventListener("click", checkAnswer);
+    var answerElements = document.querySelectorAll("li");
+    for (let x = 0; x < answerElements.length; x++) {
+      answerElements[x].addEventListener("click", checkAnswer);
+    }
+  } else {
+    //calls to create score form, replaces question with form
+    createForm();
+    questionSection.innerHTML = scoreForm.innerHTML;
   }
 }
 
@@ -61,6 +77,5 @@ function startQuiz() {
   document.getElementById("quiz-start-screen").style.display = "none";
   renderQuiz();
 }
-
 
 startbtn.addEventListener("click", startQuiz);
